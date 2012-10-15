76a3160109ed307fe952799e7d725711c9308732
hbochmann <hannes.bochmann@das-medienkombinat.de>
2012-10-15 15:08:28 +0200

    titel umbenannt

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'MK Sanitized Parameters',
+	'title' => 'Sanitize input parameters system wide',
 	'description' => 'Sanitize $_REQUEST, $_POST and $_GET before the processing of TYPO3 in backend or frontend starts. Take a look into ext_localconf.php how to configure.',
 	'category' => 'misc',
 	'author' => 'das MedienKombinat GmbH',
