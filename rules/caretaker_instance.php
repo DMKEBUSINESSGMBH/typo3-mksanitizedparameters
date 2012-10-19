a9b3e05126a9b6f3c9cf9486d9244a4532385c12
hbochmann <hannes.bochmann@das-medienkombinat.de>
2012-10-19 21:33:23 +0200

    bugfix

--- rules/caretaker_instance.php
+++ rules/caretaker_instance.php
@@ -4,9 +4,9 @@ defined('TYPO3_MODE') || die('Access denied.');
 // 	$rulesForCaretaker = array(
 // 		'st'		=> FILTER_SANITIZE_STRING,
 // 		'd' 		=> FILTER_UNSAFE_RAW,
-// 		's'
+// 		's'			=> FILTER_UNSAFE_RAW
 // 	);
 $rulesForCaretaker = 
-	unserialize('a:3:{s:2:"st";i:513;s:1:"d";i:516;i:0;s:1:"s";}');
+	unserialize('a:3:{s:2:"st";i:513;s:1:"d";i:516;s:1:"s";i:516;}');
 
 tx_mksanitizedparameters_Rules::addRulesForFrontend($rulesForCaretaker);
