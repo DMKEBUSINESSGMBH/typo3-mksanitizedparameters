65c9e1eb165df33d290edacbe8b7477dbb05e76c
hbochmann <hannes.bochmann@das-medienkombinat.de>
2012-10-22 12:48:29 +0200

    default filter nicht mehr encoded sondern magic quotes

--- rules/default.php
+++ rules/default.php
@@ -2,15 +2,15 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 // $defaultRules = array(
-// 	'default'	=> array(FILTER_SANITIZE_STRING, FILTER_SANITIZE_ENCODED)
+// 	'default'	=> array(FILTER_SANITIZE_STRING, FILTER_SANITIZE_MAGIC_QUOTES)
 // );
 $defaultRulesForFrontend = 
-	unserialize('a:1:{s:7:"default";a:2:{i:0;i:513;i:1;i:514;}}');
+	unserialize('a:1:{s:7:"default";a:2:{i:0;i:513;i:1;i:521;}}');
 
 tx_mksanitizedparameters_Rules::addRulesForFrontend($defaultRulesForFrontend);
 
 // $rulesForBackend =  array(
-// 	'default'	=> array(FILTER_SANITIZE_STRING, FILTER_SANITIZE_ENCODED)
+// 	'default'	=> array(FILTER_SANITIZE_STRING, FILTER_SANITIZE_MAGIC_QUOTES)
 // );
-$rulesForBackend = unserialize('a:1:{s:7:"default";a:2:{i:0;i:513;i:1;i:514;}}');
+$rulesForBackend = unserialize('a:1:{s:7:"default";a:2:{i:0;i:513;i:1;i:521;}}');
 tx_mksanitizedparameters_Rules::addRulesForBackend($rulesForBackend);
