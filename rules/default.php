ac5475d74e72e005abd74ff8e32352275f330a95
hbochmann <hannes.bochmann@das-medienkombinat.de>
2013-05-29 11:22:41 +0200

    default regeln für BE entfernt da unnütz

--- rules/default.php
+++ rules/default.php
@@ -25,14 +25,13 @@ $rulesForFrontend =
 tx_mksanitizedparameters_Rules::addRulesForFrontend($rulesForFrontend);
 
 // $rulesForBackend =  array(
-// 	tx_mksanitizedparameters_Rules::DEFAULT_RULES_KEY	=> array(FILTER_SANITIZE_STRING, FILTER_SANITIZE_MAGIC_QUOTES),
 // 	tx_mksanitizedparameters_Rules::COMMON_RULES_KEY	=> array(
 // 		// pid, uid und id sollten immer zahlen sein
 // 		'id' => FILTER_SANITIZE_NUMBER_INT,
 // 		'uid' => FILTER_SANITIZE_NUMBER_INT,
-//	 	'pid' => FILTER_SANITIZE_NUMBER_INT,
+//	 	'pid' => FILTER_SANITIZE_STRING,
 // 	)
 // );
- 
-$rulesForBackend = unserialize('a:2:{s:9:"__default";a:2:{i:0;i:513;i:1;i:521;}s:8:"__common";a:3:{s:2:"id";i:519;s:3:"uid";i:519;s:3:"pid";i:519;}}');
+
+$rulesForBackend = unserialize('a:1:{s:8:"__common";a:3:{s:2:"id";i:519;s:3:"uid";i:519;s:3:"pid";i:513;}}');
 tx_mksanitizedparameters_Rules::addRulesForBackend($rulesForBackend);
