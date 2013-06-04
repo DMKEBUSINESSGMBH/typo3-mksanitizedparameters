41f778f73d50669aa221e6c211001e8bcc3bfd70
Ralf Urban <urban@das-medienkombinat.de>
2013-06-04 13:17:01 +0200

    AC237: Korrektur ID-Bereinigung, da alias erlaubt sein muss

--- rules/default.php
+++ rules/default.php
@@ -7,20 +7,21 @@ defined('TYPO3_MODE') || die('Access denied.');
 // 		// Extbase request token für Formulare
 // 		// JSON String
 // 		'__hmac' => FILTER_UNSAFE_RAW,
-// 
-//		// wird nur mkforms intern verwendet
-//		// JSON String
-//		'AMEOSFORMIDABLE_ADDPOSTVARS' => FILTER_UNSAFE_RAW,
-// 
-// 		// pid, uid und id sollten immer zahlen sein
-// 		'id' => FILTER_SANITIZE_NUMBER_INT,
+
+// 		// wird nur mkforms intern verwendet
+// 		// JSON String
+// 		'AMEOSFORMIDABLE_ADDPOSTVARS' => FILTER_UNSAFE_RAW,
+
+// 		'id' => FILTER_SANITIZE_STRING,
+// 		// pid und uid sollten immer zahlen sein
 // 		'uid' => FILTER_SANITIZE_NUMBER_INT,
-//	 	'pid' => FILTER_SANITIZE_NUMBER_INT,
+// 	 	'pid' => FILTER_SANITIZE_NUMBER_INT,
 // 	)
 // );
  
+
 $rulesForFrontend = 
-	unserialize('a:2:{s:9:"__default";a:2:{i:0;i:513;i:1;i:521;}s:8:"__common";a:5:{s:6:"__hmac";i:516;s:27:"AMEOSFORMIDABLE_ADDPOSTVARS";i:516;s:2:"id";i:519;s:3:"uid";i:519;s:3:"pid";i:519;}}');
+	unserialize('a:2:{s:9:"__default";a:2:{i:0;i:513;i:1;i:521;}s:8:"__common";a:5:{s:6:"__hmac";i:516;s:27:"AMEOSFORMIDABLE_ADDPOSTVARS";i:516;s:2:"id";i:513;s:3:"uid";i:519;s:3:"pid";i:519;}}');
 
 tx_mksanitizedparameters_Rules::addRulesForFrontend($rulesForFrontend);
 
