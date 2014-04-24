45265f34fb0464eff740dd6486fd875db803117c
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-04-24 11:20:47 +0200

    pid als string bereinigen da es auch kommaseparierte Liste sein könnte

--- rules/default.php
+++ rules/default.php
@@ -13,15 +13,16 @@ defined('TYPO3_MODE') || die('Access denied.');
 // 		'AMEOSFORMIDABLE_ADDPOSTVARS' => FILTER_UNSAFE_RAW,
 
 // 		'id' => FILTER_SANITIZE_STRING,
-// 		// pid und uid sollten immer zahlen sein
-// 		'uid' => FILTER_SANITIZE_NUMBER_INT,
-// 	 	'pid' => FILTER_SANITIZE_NUMBER_INT,
+// 		// pid kann eine kommaseparierte Liste sein
+// 		'pid' => FILTER_SANITIZE_STRING,
+//		// uid sollten immer zahlen sein
+// 	 	'uid' => FILTER_SANITIZE_NUMBER_INT,
 // 	)
 // );
- 
 
-$rulesForFrontend = 
-	unserialize('a:2:{s:9:"__default";a:2:{i:0;i:513;i:1;i:521;}s:8:"__common";a:5:{s:6:"__hmac";i:516;s:27:"AMEOSFORMIDABLE_ADDPOSTVARS";i:516;s:2:"id";i:513;s:3:"uid";i:519;s:3:"pid";i:519;}}');
+
+$rulesForFrontend =
+	unserialize('a:2:{s:9:"__default";a:2:{i:0;i:513;i:1;i:521;}s:8:"__common";a:5:{s:6:"__hmac";i:516;s:27:"AMEOSFORMIDABLE_ADDPOSTVARS";i:516;s:2:"id";i:513;s:3:"pid";i:513;s:3:"uid";i:519;}}');
 
 tx_mksanitizedparameters_Rules::addRulesForFrontend($rulesForFrontend);
 
