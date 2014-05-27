15f9b234e20572bae282034298be7e3a1a44bd57
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-05-27 14:11:26 +0200

    anpassung für typo3 6.2.x

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,7 +9,7 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/index_ts.php']['preprocessRequest'][] =
 
 // sanitize in BE
 // wir setzen einen eindeutigen Key um diesen in den Tests überschreiben zu können
-$TYPO3_CONF_VARS['SC_OPTIONS']['typo3/template.php']['preStartPageHook']['mksanitizedparameters'] =
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['preStartPageHook']['mksanitizedparameters'] =
 	'EXT:'.$_EXTKEY.'/hooks/class.tx_mksanitizedparameters_hooks_PreprocessTypo3Requests.php:tx_mksanitizedparameters_hooks_PreprocessTypo3Requests->sanitizeGlobalInputArrays';
 
 require_once(t3lib_extMgm::extPath($_EXTKEY).'ext_rules.php');
