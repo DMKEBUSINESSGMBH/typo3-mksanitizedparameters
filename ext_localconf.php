9a25abce80f88e825242ae5516dffe62cd7f8b79
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2018-05-23 08:55:05 +0200

    bugfix use determineId-PostProc hook so TCA is always loaded

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ defined('TYPO3_MODE') || die('Access denied.');
 $_EXTKEY = 'mksanitizedparameters';
 
 // sanitize in FE including eID
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/index_ts.php']['preprocessRequest'][] =
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['determineId-PostProc'][] =
     'EXT:'.$_EXTKEY.'/hooks/class.tx_mksanitizedparameters_hooks_PreprocessTypo3Requests.php:tx_mksanitizedparameters_hooks_PreprocessTypo3Requests->sanitizeGlobalInputArrays';
 
 // sanitize in BE
