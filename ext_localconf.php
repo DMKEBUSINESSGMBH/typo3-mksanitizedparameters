5b2f1a39e51cf72d4674595a895e48630d3a565d
Sebastian Nürnberger <sebastian.nuernberger@dmk-ebusiness.de>
2019-04-10 11:30:54 +0200

    AC#633-3615 fix hook paths

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,11 +5,11 @@ $_EXTKEY = 'mksanitizedparameters';
 
 // sanitize in FE including eID
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['determineId-PostProc'][] =
-    'EXT:'.$_EXTKEY.'/hooks/class.tx_mksanitizedparameters_hooks_PreprocessTypo3Requests.php:tx_mksanitizedparameters_hooks_PreprocessTypo3Requests->sanitizeGlobalInputArrays';
+    'tx_mksanitizedparameters_hooks_PreprocessTypo3Requests->sanitizeGlobalInputArrays';
 
 // sanitize in BE
 // wir setzen einen eindeutigen Key um diesen in den Tests überschreiben zu können
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['preStartPageHook']['mksanitizedparameters'] =
-    'EXT:'.$_EXTKEY.'/hooks/class.tx_mksanitizedparameters_hooks_PreprocessTypo3Requests.php:tx_mksanitizedparameters_hooks_PreprocessTypo3Requests->sanitizeGlobalInputArrays';
+    'tx_mksanitizedparameters_hooks_PreprocessTypo3Requests->sanitizeGlobalInputArrays';
 
 require_once(tx_rnbase_util_Extensions::extPath($_EXTKEY).'ext_rules.php');
