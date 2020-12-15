115c78d18bac0a907d8ac8c76739a10d322a9dee
Michael Wagner <michael.wagner@dmk-ebusiness.com>
2020-12-15 17:00:18 +0100

    Add debugger and logger unit tests for sanitizer

---
+++ Tests/Classes/SanitizerDebuggerTest.php
@@ -0,0 +1,148 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DMK\MkSanitizedParameters;
+
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2020 DMK E-BUSINESS GmbH <dev@dmk-ebusiness.de>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use DMK\MkSanitizedParameters\Input\ArrayInput;
+use DMK\MkSanitizedParameters\Utility\DebugUtility;
+use DMK\MkSanitizedParameters\Utility\FilterUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * @author Michael Wagner
+ * @license http://www.gnu.org/licenses/lgpl.html
+ *          GNU Lesser General Public License, version 3 or later
+ */
+class SanitizerDebuggerTest extends AbstractTestCase
+{
+    /**
+     * @test
+     * @group unit
+     */
+    public function sanitizeInputDoesNotCallDebuggerIfDebuggingNotEnabledAndValueNotChanged()
+    {
+        // enable debug mode
+        $this->setExtConf(['debugMode' => 0]);
+        // set common rule (all a string)
+        $this->addRules([Rules::COMMON_RULES_KEY => FILTER_SANITIZE_STRING]);
+
+        $debugger = $this->prophesize(DebugUtility::class);
+        $debugger->debug()->shouldNotBeCalled();
+        GeneralUtility::setSingletonInstance(DebugUtility::class, $debugger->reveal());
+
+        $filter = $this->prophesize(FilterUtility::class);
+        $filter->isValueChanged('bar', 'bar')->willReturn(false);
+        GeneralUtility::addInstance(FilterUtility::class, $filter->reveal());
+
+        $input = Factory::createInput(ArrayInput::class, 'TestInput', ['foo' => 'bar']);
+        Factory::getSanitizer()->sanitizeInput($input);
+
+        $this->assertSame(['foo' => 'bar'], $input->getInputArray());
+    }
+
+    /**
+     * @test
+     * @group unit
+     */
+    public function sanitizeInputDoesNotCallDebuggerIfDebuggingNotEnabledAndValueChanged()
+    {
+        // enable debug mode
+        $this->setExtConf(['debugMode' => 0]);
+        // set common rule (all a string)
+        $this->addRules([Rules::COMMON_RULES_KEY => FILTER_SANITIZE_STRING]);
+
+        $debugger = $this->prophesize(DebugUtility::class);
+        $debugger->debug()->shouldNotBeCalled();
+        GeneralUtility::setSingletonInstance(DebugUtility::class, $debugger->reveal());
+
+        $filter = $this->prophesize(FilterUtility::class);
+        $filter->isValueChanged('bar', 'bar')->willReturn(true);
+        GeneralUtility::addInstance(FilterUtility::class, $filter->reveal());
+
+        $input = Factory::createInput(ArrayInput::class, 'TestInput', ['foo' => 'bar']);
+        Factory::getSanitizer()->sanitizeInput($input);
+
+        $this->assertSame(['foo' => 'bar'], $input->getInputArray());
+    }
+
+    /**
+     * @test
+     * @group unit
+     */
+    public function sanitizeInputDoesNotCallDebuggerIfDebuggingEnabledAndValueNotChanged()
+    {
+        // enable debug mode
+        $this->setExtConf(['debugMode' => 1]);
+        // set common rule (all a string)
+        $this->addRules([Rules::COMMON_RULES_KEY => FILTER_SANITIZE_STRING]);
+
+        $debugger = $this->prophesize(DebugUtility::class);
+        $debugger->debug()->shouldNotBeCalled();
+        GeneralUtility::setSingletonInstance(DebugUtility::class, $debugger->reveal());
+
+        $filter = $this->prophesize(FilterUtility::class);
+        $filter->isValueChanged('bar', 'bar')->willReturn(false);
+        GeneralUtility::addInstance(FilterUtility::class, $filter->reveal());
+
+        $input = Factory::createInput(ArrayInput::class, 'TestInput', ['foo' => 'bar']);
+        Factory::getSanitizer()->sanitizeInput($input);
+
+        $this->assertSame(['foo' => 'bar'], $input->getInputArray());
+    }
+
+    /**
+     * @test
+     * @group unit
+     */
+    public function sanitizeInputCallsDebuggerCorrectIfDebuggingEnabledAndValueChanged()
+    {
+        // enable debug mode
+        $this->setExtConf(['debugMode' => 1]);
+        // set common rule (all a string)
+        $this->addRules([Rules::COMMON_RULES_KEY => FILTER_SANITIZE_STRING]);
+
+        $debugger = $this->prophesize(DebugUtility::class);
+        $debugger->debug(
+            [
+                'Parameter Name:' => 'foo',
+                'initialer Wert:' => 'bar',
+                'Wert nach Bereinigung:' => 'bar',
+                'komplettes Parameter Array' => ['foo' => 'bar'],
+            ]
+        );
+        GeneralUtility::setSingletonInstance(DebugUtility::class, $debugger->reveal());
+
+        $filter = $this->prophesize(FilterUtility::class);
+        $filter->isValueChanged('bar', 'bar')->willReturn(true);
+        GeneralUtility::addInstance(FilterUtility::class, $filter->reveal());
+
+        $input = Factory::createInput(ArrayInput::class, 'TestInput', ['foo' => 'bar']);
+        Factory::getSanitizer()->sanitizeInput($input);
+
+        $this->assertSame(['foo' => 'bar'], $input->getInputArray());
+    }
+}
---
+++ Tests/Classes/SanitizerLoggerTest.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DMK\MkSanitizedParameters;
+
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2020 DMK E-BUSINESS GmbH <dev@dmk-ebusiness.de>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use DMK\MkSanitizedParameters\Input\ArrayInput;
+use DMK\MkSanitizedParameters\Utility\FilterUtility;
+use TYPO3\CMS\Core\Log\Logger;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * @author Michael Wagner
+ * @license http://www.gnu.org/licenses/lgpl.html
+ *          GNU Lesser General Public License, version 3 or later
+ */
+class SanitizerLoggerTest extends AbstractTestCase
+{
+    /**
+     * @test
+     * @group unit
+     */
+    public function sanitizeInputDoesNotCallLoggerIfLoggerNotEnabledAndValueNotChanged()
+    {
+        // enable debug mode
+        $this->setExtConf(['logMode' => 0]);
+        // set common rule (all a string)
+        $this->addRules([Rules::COMMON_RULES_KEY => FILTER_SANITIZE_STRING]);
+
+        $logger = $this->prophesize(Logger::class);
+        $logger->warning()->shouldNotBeCalled();
+        GeneralUtility::addInstance(Logger::class, $logger->reveal());
+
+        $filter = $this->prophesize(FilterUtility::class);
+        $filter->isValueChanged('bar', 'bar')->willReturn(false);
+        GeneralUtility::addInstance(FilterUtility::class, $filter->reveal());
+
+        $input = Factory::createInput(ArrayInput::class, 'TestInput', ['foo' => 'bar']);
+        Factory::getSanitizer()->sanitizeInput($input);
+
+        $this->assertSame(['foo' => 'bar'], $input->getInputArray());
+    }
+
+    /**
+     * @test
+     * @group unit
+     */
+    public function sanitizeInputDoesNotCallLoggerIfLoggingNotEnabledAndValueChanged()
+    {
+        // enable debug mode
+        $this->setExtConf(['logMode' => 0]);
+        // set common rule (all a string)
+        $this->addRules([Rules::COMMON_RULES_KEY => FILTER_SANITIZE_STRING]);
+
+        $logger = $this->prophesize(Logger::class);
+        $logger->warning()->shouldNotBeCalled();
+        GeneralUtility::addInstance(Logger::class, $logger->reveal());
+
+        $filter = $this->prophesize(FilterUtility::class);
+        $filter->isValueChanged('bar', 'bar')->willReturn(true);
+        GeneralUtility::addInstance(FilterUtility::class, $filter->reveal());
+
+        $input = Factory::createInput(ArrayInput::class, 'TestInput', ['foo' => 'bar']);
+        Factory::getSanitizer()->sanitizeInput($input);
+
+        $this->assertSame(['foo' => 'bar'], $input->getInputArray());
+    }
+
+    /**
+     * @test
+     * @group unit
+     */
+    public function sanitizeInputDoesNotCallLoggerIfLoggingEnabledAndValueNotChanged()
+    {
+        // enable debug mode
+        $this->setExtConf(['logMode' => 1]);
+        // set common rule (all a string)
+        $this->addRules([Rules::COMMON_RULES_KEY => FILTER_SANITIZE_STRING]);
+
+        $logger = $this->prophesize(Logger::class);
+        $logger->warning()->shouldNotBeCalled();
+        GeneralUtility::addInstance(Logger::class, $logger->reveal());
+
+        $filter = $this->prophesize(FilterUtility::class);
+        $filter->isValueChanged('bar', 'bar')->willReturn(false);
+        GeneralUtility::addInstance(FilterUtility::class, $filter->reveal());
+
+        $input = Factory::createInput(ArrayInput::class, 'TestInput', ['foo' => 'bar']);
+        Factory::getSanitizer()->sanitizeInput($input);
+
+        $this->assertSame(['foo' => 'bar'], $input->getInputArray());
+    }
+
+    /**
+     * @test
+     * @group unit
+     */
+    public function sanitizeInputCallsLoggerCorrectIfLoggingEnabledAndValueChanged()
+    {
+        // enable debug mode
+        $this->setExtConf(['logMode' => 1]);
+        // set common rule (all a string)
+        $this->addRules([Rules::COMMON_RULES_KEY => FILTER_SANITIZE_STRING]);
+
+        $logger = $this->prophesize(Logger::class);
+        $logger->warning(
+            Sanitizer::MESSAGE_VALUE_HAS_CHANGED,
+            [
+                'Parameter Name:' => 'foo',
+                'initialer Wert:' => 'bar',
+                'Wert nach Bereinigung:' => 'bar',
+                'komplettes Parameter Array' => ['foo' => 'bar'],
+            ]
+        );
+        GeneralUtility::addInstance(Logger::class, $logger->reveal());
+
+        $filter = $this->prophesize(FilterUtility::class);
+        $filter->isValueChanged('bar', 'bar')->willReturn(true);
+        GeneralUtility::addInstance(FilterUtility::class, $filter->reveal());
+
+        $input = Factory::createInput(ArrayInput::class, 'TestInput', ['foo' => 'bar']);
+        Factory::getSanitizer()->sanitizeInput($input);
+
+        $this->assertSame(['foo' => 'bar'], $input->getInputArray());
+    }
+}
