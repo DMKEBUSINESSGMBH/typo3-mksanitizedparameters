794bb3e0ab15869a0125aca6faf1c85964c2ddcf
Michael Wagner <michael.wagner@dmk-ebusiness.com>
2022-01-17 14:10:36 +0100

    Cleanup double header comment

--- Tests/Classes/Utility/ConfigurationUtilityTest.php
+++ Tests/Classes/Utility/ConfigurationUtilityTest.php
@@ -29,29 +29,6 @@ declare(strict_types=1);
 
 namespace DMK\MkSanitizedParameters\Utility;
 
-/***************************************************************
- * Copyright notice
- *
- * (c) 2020 DMK E-BUSINESS GmbH <dev@dmk-ebusiness.de>
- * All rights reserved
- *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 use DMK\MkSanitizedParameters\AbstractTestCase;
 use DMK\MkSanitizedParameters\Factory;
 use ReflectionObject;
@@ -163,9 +140,6 @@ class ConfigurationUtilityTest extends AbstractTestCase
         $extensionConfigurationProperty = $configReflection->getProperty('extensionConfiguration');
         $extensionConfigurationProperty->setAccessible(true);
         $extensionConfigurationProperty->setValue($config, null);
-
-        // legacy configuration
-        $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['mksanitizedparameters'] = serialize($configuration);
 
         // config loading for typo3 9 or later
         if (Typo3Utility::isTypo3Version9OrHigher()) {
